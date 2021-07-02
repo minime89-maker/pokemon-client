e6cbb0ead002677c8ee60c652637451598099f80
Minja Popovic <mini.me@mein.gmx>
2021-07-02 15:26:43 +0200

    Creating structure for the app¨, api connected and basic material components implemented

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,46 +1,121 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router' 
 import { useHistory } from 'react-router-dom'
+import Loading from './Loading'
+import Error from './Error'
+import { makeStyles, Grid, Card, CardContent, Button, Typography, CardActions, CardHeader, FormControlLabel, Fade, Switch, Paper } from '@material-ui/core'
+
+const useStyles = makeStyles({
+	card: {
+		minWidth: 380,
+		maxWidth: 400,
+		margin: '10% auto',
+		padding: 30,
+		textAlign: 'center',
+	},
+	buttons: {
+		display: 'flex',
+		alingItems: 'center',
+		justifyContent: 'center',
+		marginTop: '40px'
+	},
+	types: {
+		padding: '10px 0',
+	},
+	more: {
+		marginTop: '30px',
+		padding: 10
+	}
+})
 
 const Pokemon = () => {
 
 	const [pokemon, setPokemon] = useState()
+	const [loading, setLoading] = useState(null)
+	const[error, setError] = useState()
 	const { id } = useParams()
 	const history = useHistory()
+	const classes = useStyles()
+	const [checked, setChecked] = useState(false)
+	const [data, setData] = useState(null)
+
+	const handleChange = () => {
+		setChecked((prev) => !prev)
+	}
 
 	useEffect(() => {
+		setLoading(true)
+		setError(false)
 		axios.get(`http://localhost:3000/pokemon/${id}`)
 			.then((res) => {
 				console.log(res.data)
 				setPokemon(res.data)
+				setLoading(false)
 			})
-			.catch((err) => console.log(err))
+			.catch((err) => {
+				console.log(err)
+				setError(true)
+				setLoading(false)
+			})
 	}, [id])
 
+	useEffect(() => {
+		setLoading(true)
+		setError(false)
+		 const fetchData= async () => {
+		  const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+			.then((result) => {
+				console.log(result)
+				setData(result.data)
+				setLoading(false)
+			})
+			.catch((err) => {
+				console.log(err)
+				setError(true)
+				setLoading(false)
+			})
+		}
+		fetchData();
+	  }, [id]);
+
+	  
 	return (
-		<div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
-		<button onClick={() => history.goBack()} style={{margin: '20px auto'}}>Go Back</button>
-			{pokemon && (
-				<div >
-					<h1>Name: {pokemon.name.english}</h1>
-					<div style={{color: 'red'}}>
-						<h2>Type: </h2> 
-						{pokemon.type && pokemon.type.map((type, index) => (
-							<div key={index}>
-								<p> {type}</p>
-							</div>
-						))}
+		<Fragment>
+		<Grid container>
+			{loading && <Loading />}
+			{error && <Error />}
+				{pokemon && (
+					<Card elevation={10} className={classes.card}>
+					<div>
+						{data && (
+							<img src={data.sprites.front_default} />
+						)}
 					</div>
-					<div style={{color: 'blue', cursor: 'pointer'}}>
-						<h2 onClick={() => history.push(`/pokemon/${pokemon.id}/info`)}>Base: </h2>
-						<h3>Attack: {pokemon.base.Attack}</h3>
-						<h3>Defense: {pokemon.base.Defense}</h3>
-					</div>
-					
-				</div>
-			)}
-		</div>
+						<Typography variant="h3">{pokemon.name.english}</Typography>
+						<FormControlLabel className={classes.buttons} control={
+								<Switch checked={checked} 
+								onChange={handleChange} />
+							} 
+								label='Type'
+							/>
+								{pokemon.type && pokemon.type.map((type, index) => (
+									<Fade in={checked} key={index}>
+										<Typography className={classes.types}>{type}
+										</Typography>
+									</Fade>
+								))}
+								<Button className={classes.more} color='primary' onClick={() => history.push(`/pokemon/${pokemon.id}/info`)}>
+									Base
+								</Button>
+								{/* <h3>Attack: {pokemon.base.Attack}</h3>
+								<h3>Defense: {pokemon.base.Defense}</h3> */}							
+					</Card>
+				)}
+		</Grid>
+
+		<Button style={{position: 'absolute', left: '45%'}} onClick={() => history.goBack()}>Go Back</Button>
+		</Fragment>
 	)
 }
 
--- src/components/Pokemons.js
+++ src/components/Pokemons.js
@@ -1,34 +1,84 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
-import { useHistory } from 'react-router-dom'
+import { useHistory, useParams } from 'react-router-dom'
+import Loading from './Loading'
+import Error from './Error'
+import { makeStyles, Grid, Card, CardContent, Button, Typography, CardActions, Avatar, CardHeader } from '@material-ui/core'
+
+const useStyles = makeStyles({
+	cards: {
+		minWidth: 380,
+		maxWidth: 400,
+		margin: '20px auto',
+		textAlign: 'left'
+	},
+	button: {
+		color: '#222',
+		margin: '0 auto',
+		padding: '8px 16px'
+	},
+	names: {
+		textAlign: 'center',
+	}
+})
+
 
 
 const Pokemons = () => {
 
-	const [pokemons, setPokemons] = useState([])
+	const [pokemons, setPokemons] = useState(null)
+	const [loading, setLoading] = useState(true)
+	const[error, setError] = useState(false)
 	const history = useHistory()
+	const classes = useStyles()
+
+	
 
 	useEffect(() => {
+		setLoading(true)
+		setError(false)
 		axios.get('http://localhost:3000/pokemon')
 			.then((res) => {
 				console.log(res.data[1].name)
 				setPokemons(res.data)
+				setLoading(false)
 			})
-			.catch((err) => console.log(err))
+			.catch((err) => {
+				console.log(err)
+				setError(true)
+				setLoading(false)
+			})
 	}, [])
 
 	return (
-		<div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', cursor: 'pointer'}}>
-			{pokemons && pokemons.map((poke) => (
-					<ul onClick={() => history.push(`/pokemon/${poke.id}`)} key={poke.id}>
-						<li>{poke.name.english}</li>
-						<li>{poke.name.japanese}</li>
-						<li>{poke.name.french}</li>
-						<li>{poke.name.chinese}</li>
-					</ul>
-				)
-			)})
-		</div>
+		<Grid container>
+			{loading && <Loading />}
+			{error && <Error />}
+				{pokemons && pokemons.map((poke) => (
+						<Card elevation={1} className={classes.cards} key={poke.id}>
+						<CardHeader avatar={
+							<Avatar>{poke.name.english[0]}</Avatar>
+						}
+						title={
+							<Typography variant='h5'>{poke.name.english}</Typography>
+						}
+						>
+						</CardHeader>
+							<CardContent className={classes.names}>
+								<Typography variant='body2' style={{paddingBottom: '10px'}}>{poke.name.japanese}</Typography>
+								<Typography variant='body2' style={{paddingBottom: '10px'}}>{poke.name.french}</Typography>
+								<Typography variant='body2'>{poke.name.chinese}</Typography>
+							</CardContent>
+								<CardActions>
+									<Button className={classes.button} onClick={() => history.push(`/pokemon/${poke.id}`)}>
+										Info
+									</Button>
+								</CardActions>
+						</Card>
+					)
+				)})
+				
+		</Grid>
 	)
 }
 
